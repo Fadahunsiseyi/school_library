8f116a4c1a3bdd4ad662670d837909d5461cd5a6
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-09-27 07:33:57 +0100

    add teacher file

---
+++ teacher.rb
@@ -0,0 +1,14 @@
+relative_require 'Person'
+
+class Teacher < Person
+    attr_accessor :specialization
+
+    def initialize(name="Unknown", age, specialization, parent_permission: true)
+        super(name, age, parent_permission)
+        @specialization = specialization
+    end
+
+    def can_use_services?
+        true
+    end
+end
