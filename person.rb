8bbb3a7fbfdcda1aa09ccc38ec61a01f08d8d975
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-09-27 06:21:21 +0100

    Add the private method with its conditionals

--- person.rb
+++ person.rb
@@ -2,5 +2,16 @@ class Person
     attr_accessor :name, :age, :parent_permission
     attr_reader :id
     def initialize(name="Unknown", age, parent_permission: true)
+        @name = name
+        @age = age
+        @parent_permission = parent_permission
+    end
+    Private
+    
+    def is_of_age?
+        if @age >= 18
+            true
+        else 
+            false
     end
 end
