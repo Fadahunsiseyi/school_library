804a0c4bcf3a7a4688e459e5fda80b8592d67dcc
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-09-29 22:31:33 +0100

    format document

--- classroom.rb
+++ classroom.rb
@@ -1,6 +1,7 @@
 class Classroom
-    attr_accessor :label
-    def initialize(label)
-        @label = label
-    end
+  attr_accessor :label
+
+  def initialize(label)
+    @label = label
+  end
 end
